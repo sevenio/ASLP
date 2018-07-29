f647aae3c31874214a48b3df0844120b174cbd06
sevenio <seveniotacheon@gmail.com>
2018-07-29 18:52:22 +0530

    add image resources and resolv aapt2 error caused due to usage of keyboard as widget id in xml file

---
+++ app/src/main/java/com/example/gangadhar/aslpapp/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.gangadhar.aslpapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+
+public class MainActivity extends AppCompatActivity {
+
+   private RecyclerView mRecyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+    private void initUi(){
+        mRecyclerView = findViewById(R.id.articulation_recyclerview);
+
+
+    }
+}
